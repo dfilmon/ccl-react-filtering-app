0209c9dae5cd25202bd58abb275605e94d967def
dfilmon <abraha.filmon@gmail.com>
2022-12-02 18:43:05 -0500

    2 files modified, 1 added

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Card() {
+  return (
+    <div>
+      <div class="column nature">
+        <img
+          src="https://images.unsplash.com/photo-1664575262619-b28fef7a40a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt="Mountains"
+          style={{ width: '100%' }}
+        />
+        <div class="content">
+          <h4>Mountains</h4>
+          <p>Lorem ipsum dolor..</p>
+        </div>
+      </div>
+    </div>
+  );
+}
