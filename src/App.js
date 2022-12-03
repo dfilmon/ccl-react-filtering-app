a94faca73ea4ea4944531e70fc3c191e3ce205aa
dfilmon <abraha.filmon@gmail.com>
2022-12-02 19:09:33 -0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import Card from './components/Card';
 export default function App() {
   return (
     <div>
-      <Card imgURL="https://tinyurl.com/mw4s3sz6" />
-      <Card imgURL="https://tinyurl.com/5b7prhvk" />
-      <Card imgURL="https://tinyurl.com/2p8zzx7b" />
+      <Card
+        imgURL="https://tinyurl.com/mw4s3sz6"
+        title="Work"
+        description="Description goes here"
+      />
+      <Card
+        imgURL="https://tinyurl.com/5b7prhvk"
+        title="Live"
+        description="Description goes here"
+      />
+      <Card
+        imgURL="https://tinyurl.com/2p8zzx7b"
+        title="Socialize"
+        description="Description goes here"
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,8 @@ export default function Card(props) {
       <div class="column nature">
         <img src={props.imgURL} alt="Mountains" style={{ width: '100%' }} />
         <div class="content">
-          <h4>Mountains</h4>
-          <p>Lorem ipsum dolor..</p>
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
         </div>
       </div>
     </div>
