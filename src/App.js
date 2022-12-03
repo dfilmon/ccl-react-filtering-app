a9a1af5c1d40ec625c7263d7127b5f80557d0e9e
dfilmon <abraha.filmon@gmail.com>
2022-12-02 19:06:11 -0500

    3 files modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Card from './components/Card';
 export default function App() {
   return (
     <div>
-      <Card />
+      <Card imgURL="https://tinyurl.com/mw4s3sz6" />
+      <Card imgURL="https://tinyurl.com/5b7prhvk" />
+      <Card imgURL="https://tinyurl.com/2p8zzx7b" />
     </div>
   );
 }
