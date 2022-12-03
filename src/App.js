8ec42457491cf9983c59503a99dcc151657b815b
dfilmon <abraha.filmon@gmail.com>
2022-12-03 09:18:24 -0500

    4 files modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import Card from './components/Card';
 export default function App() {
   return (
     <div>
-      <button class="rounded-full ...">Save Changes</button>
+      <div id="buttonContainer">
+        <button
+          type="submit"
+          className="mt-10 flex  items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Pray
+        </button>
+
+        <button
+          type="submit"
+          className="mt-10 flex  items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Code
+        </button>
+
+        <button
+          type="submit"
+          className="mt-10 flex  items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Design
+        </button>
+      </div>
 
       <Card
         imgURL="https://tinyurl.com/mw4s3sz6"
